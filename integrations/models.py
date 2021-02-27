716102fb3b8a58f7117cf21a8095ee76e285c601
glassthunder <ezamabusiness@gmail.com>
2021-02-26 17:01:18 -0700

    added some rudimentary models, along with base API class

--- integrations/models.py
+++ integrations/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class Account(Models.model):
+    account_name = models.charField(max_length=20) #note: not the same as user_name in Users model
+    user = models.ForeignKey(User)
+    user_id = models.CharField(max_length=10, default=user.user_id, primary_key=True)
+    token = models.CharField(max_length=2048)
+    refresh_token = models.CharField(max_length=512, blank=True)
+    redirect_uri = models.CharField(max_length=500)
