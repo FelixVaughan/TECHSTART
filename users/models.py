dc6f8c51bf16dbb07a3540c97ebf60abb2bfbe0c
glassthunder <ezamabusiness@gmail.com>
2021-02-28 11:13:04 -0700

    more fixes

--- users/models.py
+++ users/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class User(models.Model):
-    user_name = models.CharField(max_length = 30, on_delete=models.CASCADE, primary_key = True)
+    user_name = models.CharField(max_length = 30, unique=True)
     date_joined = models.DateTimeField(auto_now=True)
     password = models.CharField(max_length = 40)
     user_id = models.CharField(max_length = 10, primary_key = True)
-    user_email = models.charField(max_length = 40, primary_key = True)
+    user_email = models.CharField(max_length = 40)
 
 
