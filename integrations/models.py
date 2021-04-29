57e7651e7abff765ffe88458bc91c0f143ccde52
Kieran Wood <kieranw098@gmail.com>
2021-04-29 01:00:36 -0600

    Added outlook fix and fixed merge conflict

--- integrations/models.py
+++ integrations/models.py
@@ -9,6 +9,7 @@ import os
 import django
 import json
 import urllib
+from datetime import datetime, timedelta
 from django.contrib.auth.models import User
 import requests
 import tekore  # spotiy api
@@ -17,8 +18,9 @@ from time import sleep
 from tempfile import NamedTemporaryFile
 from praw.util.token_manager import FileTokenManager
 from O365 import Account
+from urllib.parse import urlencode
+from django.contrib.sessions.models import Session
 
-#possibly make part of a general handler
 def local_code_flow():
     waittime = 0
     code = ""
@@ -44,7 +46,7 @@ def local_code_flow():
 class User_Account_Info(models.Model):
     account_name = models.CharField(max_length=20)  # spotify, facebook, etc. Set by default in subclass implementation constructors
     account_user_name = models.CharField(max_length=30, default="")
-    users = models.ForeignKey(User, on_delete=models.CASCADE, default='1')
+    # users = models.ForeignKey(User, on_delete=models.CASCADE, default='1')
     user_id = models.CharField(max_length=10)
     token = models.CharField(max_length=2048, default="")
     refresh_token = models.CharField(max_length=512, blank=True)
@@ -123,7 +125,7 @@ class Api:
         self.base_url = info_class().base_url
         self.token_endpoint = info_class().token_endpoint
         self.scope = info_class().scope
-        
+
 
     def init_contact(self):  # for first authentication
         api = OAuth2Session(self.client_id, scope=self.scope, redirect_uri=self.redirect_uri)
@@ -165,25 +167,24 @@ class Api:
 
 
 class Spotify_User_Info(User_Account_Info):
-    """Creates a Spotify specific User_Account_Info class to store a user's info
-    into a table"""
     account_name = models.CharField(
         max_length=7, default="spotify", editable=False)
-
+    users = models.OneToOneField(User, on_delete=models.CASCADE, default='1')
 
 class Reddit_User_Info(User_Account_Info):
-    """Creates a Reddit specific User_Account_Info class to store a user's info
-    into a table"""
     account_name = models.CharField(
         max_length=6, default="reddit", editable=False)
+    users = models.OneToOneField(User, on_delete=models.CASCADE, default='1')
 
 
 class Discord_User_Info(User_Account_Info):
-    """Creates a Discord specific User_Account_Info class to store a user's info
-    into a table"""
     account_name = models.CharField(
         max_length=7, default="discord", editable=False)
 
+class Outlook_User_Info(User_Account_Info):
+    account_name = models.CharField(
+        max_length=7, default="outlook", editable=False)
+    users = models.OneToOneField(User, on_delete=models.CASCADE, default='1')
 
 class Outlook_User_Info(User_Account_Info):
     """Creates a Outlook specific User_Account_Info class to store a user's info
@@ -191,64 +192,43 @@ class Outlook_User_Info(User_Account_Info):
     account_name = models.CharField(
         max_length=7, default="outlook", editable=False)
 
-class SpotifyApi(Api):
-    """The specific implementation for the spotify api
+class News_User_Info(User_Account_Info):
+    account_name = models.CharField(
+        max_length=7, default="newsapi", editable=False)
+    preferences = models.CharField(max_length=100, default="")
 
-    Notes
-    -----
-    - See SpotifyApi.init_contact() and SpotifyApi.contact_api() for details 
-        about class usage
-    """
+class SpotifyApi(Api):
     def __init__(self, user_id, api_name="spotify"):
         super().__init__(user_id, api_name, SpotifyAPIInfo)
         self.current_user = Spotify_User_Info.objects.get(user_id=user_id)
         self.token = self.current_user.token
         self.refresh_token = self.current_user.refresh_token
         self.conf = (self.client_id, self.client_secret, self.redirect_uri)
+        self.spotify_auth = tekore.RefreshingCredentials(client_id=self.client_id, client_secret=self.client_secret, redirect_uri=self.redirect_uri)
+        self.spotify = None
+        if(self.current_user.token):
+            self.spotify = tekore.Spotify(self.current_user.token)
 
     def init_contact(self):
-        """Initializes contact with the API
-
-        Examples
-        --------
-        ### Creating a user in django shell and initializing API
-        ```
-        import random
-        import string
-        from integrations.models import *
-        from django.contrib.auth.models import User
-
-        # Create user
-        ran_name = lambda n: ''.join([random.choice(string.ascii_lowercase) for i in range(n)])
-        user=User.objects.create_user(ran_name(random.randint(0, 10)), password='bar')
-        user.save()
-        
-        # Add user to Spotify_User_Info table
-        entry = Spotify_User_Info(user_id=user.id, account_name="yeet")
-        entry.save()
-        user.spotify_user_info_set.add(entry)
-        user.save()
-
-        # Initialize and access the spotify api
-        spot = SpotifyApi(user.id)
-        spot.init_contact()
-
-        ... # need to wait to paste url and finalize initialization 
-        """
         try:
-            spotify = tekore.RefreshingCredentials(client_id=self.client_id, client_secret=self.client_secret, redirect_uri=self.redirect_uri)
-            auth_url = spotify.user_authorisation_url(scope=self.scope)
+            auth_url = self.spotify_auth.user_authorisation_url(scope=self.scope)
             webbrowser.open(auth_url)
-            code = local_code_flow()
-            access_token = spotify.request_user_token(code)
-            self.current_user.token = access_token
-            self.current_user.refresh_token = access_token.refresh_token
-            self.current_user.save()
         except KeyError as e:
             print("Authentication with spotify API could NOT be completed as no code was found. Access token NOT set!")
         except Exception as e:
             print(e)
-
+    
+    def obtain_token(self, code):
+        try:
+            access_token = self.spotify_auth.request_user_token(code)
+            self.current_user.token = access_token
+            self.current_user.refresh_token = access_token.refresh_token
+            self.current_user.save()
+            print(f"spotify token set to {access_token}")
+        except KeyError as e:
+            print("Authentication with spotify API could NOT be completed as no code was found. Access token NOT set!")
+        except Exception as e:
+            print(e)
 
     def contact_api(self, album_uri:str = "") -> dict:  # will def need parameters in the future
         """Contacts Spotify API and returns a dictionary of values
@@ -323,23 +303,47 @@ class SpotifyApi(Api):
             # Set object attributes
             user_values["top_artist"] = spotify.current_user_top_artists(limit=1).items[0]
             #here
+            print(user_values)
             return user_values
         except tekore.ServiceUnavailable as err:
             print("It looks like you are currently not logged into spotify...")
         except Exception as e:
-            self.get_new_token(true)
+            self.get_new_token(True)
 
     def get_new_token(self,retry): #retry is used to try a failed api contact
         spotify = tekore.RefreshingCredentials(client_id=self.client_id, client_secret=self.client_secret, redirect_uri=self.redirect_uri)
-        new_access_token = spotify.refresh_user_token(self.current_user.refresh_token)
-        self.current_user.token = new_access_token
-        self.current_user.refresh_token = new_access_token.refresh_token
-        self.current_user.save()
-        if(retry):
-            try:
-                self.contact_api()
-            except Exception as e:
-                print(e)
+        if(self.current_user.refresh_token):
+            new_access_token = spotify.refresh_user_token(self.current_user.refresh_token)
+            self.current_user.token = new_access_token.access_token
+            self.current_user.refresh_token = new_access_token.refresh_token
+            self.current_user.save()
+            if(retry):
+                try:
+                    self.contact_api()
+                except Exception as e:
+                    print(e)
+        else:
+            return -1  
+
+    def change_volume(self,amount):
+        self.spotify.playback_volume(amount)
+
+    def next(self):
+        self.spotify.playback_next()
+
+    def shuffle(self):
+        self.spotify.playback_shuffle()
+
+    def prev(self):
+        self.spotify.playback_previous()
+
+    def pause(self):
+        self.spotify.playback_pause()
+
+    def play(self):
+        self.spotify.playback_resume()
+
+    
         
 
 
@@ -349,7 +353,8 @@ class RedditApi(Api):
         self.current_user = Reddit_User_Info.objects.get(user_id=user_id)
         self.token = self.current_user.token
         self.refresh_token = self.current_user.refresh_token
-
+        self.reddit = praw.Reddit(client_id=self.client_id, client_secret=self.client_secret, redirect_uri=self.redirect_uri, user_agent="techstart")
+        
     def init_contact(self):
         """Initializes contact with the API
 
@@ -377,14 +382,14 @@ class RedditApi(Api):
         red = RedditApi(user.id)
         reddit, reddit_user = red.init_contact()
         """
-        reddit = praw.Reddit(client_id=self.client_id, client_secret=self.client_secret, redirect_uri=self.redirect_uri, user_agent="techstart")
-        auth_url = reddit.auth.url(["*"], "permanent")
+        auth_url = self.reddit.auth.url(["*"], "permanent")
         webbrowser.open(auth_url)
-        code = local_code_flow()
-        refresh_token =  reddit.auth.authorize(code)
+        
+    def obtain_token(self, code):
+        refresh_token =  self.reddit.auth.authorize(code)
         self.current_user.refresh_token = refresh_token
         self.current_user.save()
-
+        print(f"reddit Token set: {refresh_token}")
     
     def contact_api(self):
         """Contacts the API and gets the user data
@@ -479,13 +484,15 @@ class DiscordApi(Api):
         self.refresh_token = self.current_user.refresh_token #set to blank in parent class. Has to be set here
 
     def init_contact(self):
-        auth_url = 'https://discord.com/api/oauth2/authorize?client_id=829140725307932733&redirect_uri=http%3A%2F%2F127.0.0.1%3A8000%2Fapi%2Fredirect&response_type=code&scope=email%20connections%20rpc%20rpc.notifications.read%20rpc.activities.write%20messages.read'
+        auth_url = self.base_url
         webbrowser.open(auth_url)
-        code = local_code_flow()
-        token_json = self.obtain_token(code) 
-        print(token_json)
-        self.token = token_json["access_token"]
-        self.refresh_token = token_json["refresh_token"]
+        # code = local_code_flow()
+        # token_json = self.obtain_token(code) 
+        # self.token = token_json["access_token"]
+        # self.refresh_token = token_json["refresh_token"]
+        # self.current_user.token = token_json["access_token"]
+        # self.current_user.refresh_token = token_json["refresh_token"]
+        # self.current_user.save()
 
     def obtain_token(self,code):
         data = {
@@ -494,12 +501,12 @@ class DiscordApi(Api):
             'grant_type': 'authorization_code',
             'code': code,
             'redirect_uri': self.redirect_uri,
-            'scope': 'email connections rpc rpc.notifications.read rpc.activities.write messages.read'
+            'scope': self.scope
         }
         headers = {
             'Content-Type': 'application/x-www-form-urlencoded'
         }
-        r = requests.post('https://discord.com/api/oauth2/token', data=data, headers=headers)
+        r = requests.post(self.token_endpoint, data=data, headers=headers)
         r.raise_for_status()
         print(json.dumps(r.json()))
         return r.json()
@@ -508,12 +515,27 @@ class DiscordApi(Api):
         pass
         #Kieran
 
-    def get_new_token():
-        pass
-        #Hrithvik 
+    def get_new_token(self):
+        data = {
+        'client_id': self.client_id,
+        'client_secret': self.client_secret,
+        'grant_type': 'refresh_token',
+        'refresh_token': self.refresh_token
+        }
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        r = requests.post(self.token_endpoint, data=data, headers=headers)
+        r.raise_for_status()
+        response = r.json()
+        self.current_user.token = response['access_token']
+        self.current_user.refresh_token = response['refresh_token']
+        self.current_user.save()
+
+        
 
 class OutlookApi(Api):
-    def __init__(self, user_id, api_name="discord"):
+    def __init__(self, user_id, api_name="outlook"):
         super().__init__(user_id, api_name, OutlookAPIInfo)
         self.current_user = Outlook_User_Info.objects.get(user_id=user_id)
         self.token = self.current_user.token #set to blank in parent class. Has to be set here
@@ -544,7 +566,6 @@ class OutlookApi(Api):
         # token = r.json().get('access_token')
         # return token
         ...
-
 
     def contact_api(self):
         """Contacts the API and gets the user data
@@ -599,12 +620,102 @@ class OutlookApi(Api):
 
         inbox = account.mailbox().inbox_folder()
         data["inbox"] = [email for email in inbox.get_messages()] # Pulls first 25 emails from inbox into list
-        return data
 
-    def get_new_token(self):
-        pass
+    def get_new_token(self, retry):
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        payload = {
+            "grant_type": "refresh_token",
+            "client_id": self.client_id,
+            "scope": self.scope,
+            "refresh_token": self.refresh_token,
+            "redirect_uri": self.redirect_uri,
+            "client_secret": self.client_secret  
+        }
+        r = requests.post(self.token_endpoint, headers=headers, data=payload)
+        response = r.json()
+        if ('access_token' in response):
+            self.current_user.token = response['access_token']
+            self.current_user.refresh_token = response['refresh_token']
+            self.current_user.save()
+        else:
+            print("could not retrieve new token")
+    
+        if(retry):
+            self.contact_api()
 
-#anay
+
+class NewsApi(Api):
+    news_endpoint = "https://newsapi.org/v2/everything"
+    apiKey = "cce67af05ae04866ad9820f0edbdf363"
+    def __init__(self, user_id, api_name="newsapi"):
+        super().__init__(user_id, api_name, NewsApiInfo)
+        self.current_user = News_User_Info.objects.get(user_id=user_id)
+
+    def get_user_articles(self,article):
+        now = datetime.now()
+        day_past = now - timedelta(hours = 24)
+        now = now.isoformat()
+        day_past = day_past.isoformat()
+        params = {
+            'q': article,
+            'from': str(day_past),
+            'to': str(now),
+            'sortBy': 'popularity',
+            'apiKey': self.apiKey 
+        }
+
+        r = requests.get(self.news_endpoint, params=params)
+        result = r.json()
+        if (result['status'] == 'ok'):
+            return result
+        else:
+            return ""
+
+    def contact_api(self):
+        preferences = self.current_user.preferences.split("  ")
+        urls = {}
+        for pref in preferences:
+            result = self.get_user_articles(pref)
+            articles = result.get('articles')
+            for article in articles:
+                if pref in urls:
+                    urls[pref].append(article['url'])
+                else:
+                     urls[pref] = [article['url']]
+        print(urls)
+        return urls #returns dict of form {preference: url}
+
+    def search_news(self, keywords, sources):
+        now = datetime.now()
+        day_past = now - timedelta(hours = 24)
+        params = {
+            'q': urllib.parse.urlencode(keywords),
+            'from': now,
+            'to': day_past,
+            'sortBy': 'popularity',
+            'apiKey': self.apiKey,
+            'sources': sources, #comma separated
+        }
+        r = requests.get(self.news_endpoint, params=params)
+        result = r.json()
+        if result['status'] == 'ok':
+            return result
+        else:
+            return "search couldn't be performed as specified"
+
+        
+
+
+    def add_prefs(self,str): #this should be moved to a form
+        if (self.current_user.preferences):
+            self.current_user.preferences = self.current_user.preferences + "  " + str.strip()
+        else:
+            self.current_user.preferences = str.strip()
+        self.current_user.save()
+
+
 class SpotifyAPIInfo(ApiInfo):
     """The spotify specific ApiInfo subclass"""
     def __init__(self):
@@ -626,7 +737,7 @@ class RedditAPIInfo(ApiInfo):
         self.base_url = "https://www.reddit.com/api/v1/authorize"
         self.token_endpoint = "https://www.reddit.com/api/v1/access_token"
         self.redirect_url = "http://127.0.0.1:8000/api/redirect"
-        self.scope = {} #TODO: Determine scope settings; possibly {'edit':True}
+        self.scope = {} #TODO: 
         # self.scope 
 
         
@@ -636,10 +747,10 @@ class DiscordAPIInfo(ApiInfo):
         self.api_name = "discord"
         self.client_id = '829140725307932733'
         self.secret = "FNAo1rbbdhLD9pda2SyaIdRl2wC2_ATn" #TODO: 
-        self.base_url = 'https://discord.com/api/oauth2/authorize'
+        self.base_url = 'https://discord.com/api/oauth2/authorize?client_id=829140725307932733&redirect_uri=http%3A%2F%2F127.0.0.1%3A8000%2Fapi%2Fredirect&response_type=code&scope=email%20connections%20rpc%20rpc.notifications.read%20rpc.activities.write%20messages.read'
         self.token_endpoint = 'https://discord.com/api/oauth2/token'
         self.redirect_url = 'http://127.0.0.1:8000/api/redirect'
-        self.scope = {} 
+        self.scope = 'email connections rpc rpc.notifications.read rpc.activities.write messages.read'
         # self.scope 
 
 class OutlookAPIInfo(ApiInfo):
@@ -648,9 +759,19 @@ class OutlookAPIInfo(ApiInfo):
         self.api_name = "outlook"
         self.client_id = "9bb0ebfa-b59b-4717-af05-506e0188c0bb"
         self.secret = "w.0ywb5.VL3VxQ~cCkEs~G6p-c8i_~-Q9~" 
-        self.base_url = "https://login.microsoftonline.com/consumers/oauth2/v2.0/authorize"
+        self.base_url = "https://login.microsoftonline.com/consumers/oauth2/v2.0/authorize?client_id=9bb0ebfa-b59b-4717-af05-506e0188c0bb&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A8000%2Fapi%2Fredirect&response_mode=query&scope=https://graph.microsoft.com/.default"
         self.token_endpoint = 'https://login.microsoftonline.com/consumers/oauth2/v2.0/token'
         self.redirect_url = '"https://login.microsoftonline.com/common/oauth2/nativeclient"' 
         self.scope = {} 
         # self.scope 
 
+class NewsApiInfo(ApiInfo):
+    def __init___(self):
+        self.api_name = "newsapi"
+        self.secret = "N/A"
+        self.client_id = "N/A"
+        self.base_url = "N/A"
+        self.token_endpoint = "N/A""N/A"
+        self.redirect_url = "N/A"
+        self.scope = "N/A"
+        # self.scope 
