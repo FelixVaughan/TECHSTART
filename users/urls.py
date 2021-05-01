b17c97bdccac28eebb29dbfa93674e9528f197e4
smitra33 <60757186+smitra33@users.noreply.github.com>
2021-04-30 20:43:06 -0600

    Added extra button functionality to Spotify container, Frontend now calls: spot.init_contact() and spot.contact_api() with buttons

--- users/urls.py
+++ users/urls.py
@@ -6,6 +6,7 @@ from . import views
 
 urlpatterns = [
     url(r"^$", views.index, name = "index"), # site hompage
+    path('ajax/', views.ajax, name = 'ajax'),
     path('register/', views.register, name = 'register'),   # register page path
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'), # login page
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'), # logout page
