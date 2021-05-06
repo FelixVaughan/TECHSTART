6c1ef211341ce89bb057bc94903f437d118cea32
g-sohi <gpsohi@hotmail.com>
2021-05-05 23:03:48 -0600

    connected contact page

--- users/urls.py
+++ users/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('song/', views.song, name='song'),
     path('article/', views.article_overview, name='article'),
     path('register/', views.register, name='register'),   # register page path
+    path('contact/', views.contact_page, name='contact'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'),
          name='login'),  # login page
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'),
